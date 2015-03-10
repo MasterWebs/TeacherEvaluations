6c1923f73e6a6509b234c3a7bca635b1f6f137cb
Snaebjorn Eyjolfsson <snabbie1@gmail.com>
2015-03-10 18:38:58 +0000

    added toastr as dependency, also error msg's for login

--- src/LoginController.js
+++ src/LoginController.js
@@ -1,12 +1,22 @@
 angular.module('EvalApp').controller('LoginController', 
-['$scope', '$location', '$rootScope', '$routeParams',
-function ($scope, $location, $rootScope, $routeParams) {
+['$scope', '$location', '$rootScope', '$routeParams', 'LoginResource',
+function ($scope, $location, $rootScope, $routeParams, LoginResource) {
 	$scope.user = '';
 	$scope.pass = '';
 
 	$scope.login = function () {
 		// TODO:
 		// login user with REST service
-
+		if ($scope.user !== '' && $scope.pass !== '') {
+			LoginResource.login($scope.user, $scope.pass);
+		} else {
+			if ($scope.user === '' && $scope.pass === '') {
+				toastr.error('Username and password cannot be empty!', 'Login error');
+			} else if ($scope.user === '') {
+				toastr.error('Username cannot be empty!', 'Login error');
+			} else {
+				toastr.error('Password cannot be empty!', 'Login error');
+			}
+		}
 	};
 }]);
