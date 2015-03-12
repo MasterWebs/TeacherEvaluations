cbe72f8980ab3b238988bd612c13fb6086902078
omaroskars <omar13@ru.is>
2015-03-12 12:54:11 +0000

    getting pull

--- src/MyResource.js
+++ src/MyResource.js
@@ -3,9 +3,11 @@ function ($http, SERVER_URL) {
 	var token = '';
 	var config = '';
 	return {
-		courses: function (tok) { 
+		init: function (tok) {
 			token = 'Basic ' + tok;
 			config = {headers:{'Authorization': token}};
+		},
+		courses: function () { 
 			return $http.get(SERVER_URL + 'my/courses', config); }		
 
 	};
