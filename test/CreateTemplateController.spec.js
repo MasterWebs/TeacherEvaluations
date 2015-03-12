0df6da0ddf677aa0bbdcaa6419b8dc3f148bb0be
omaroskars <omar13@ru.is>
2015-03-12 22:39:45 +0000

    updated test for create template

--- test/CreateTemplateController.spec.js
+++ test/CreateTemplateController.spec.js
@@ -5,6 +5,72 @@ describe('CreateTemplateController', function () {
 
 	var $controller;
 
+	var mockLoginResource = {
+		login: function (user, pass) {
+			return {
+				success: function (fn) {
+					var response = {};
+					if (user === 'snaebjorn13' && pass === '123456') {
+						response = {
+							Token: 'ajfsjsafjab',
+							User: {
+								Username: 'snaebjorn13',
+								Role: 'student'
+							}
+						};
+						fn(response);
+					} else if (user === 'admin' && pass === '123456') {
+						response = {
+							Token: 'jafjsaja',
+							User: {
+								Username: 'admin',
+								Role: 'admin'
+							}
+						};
+						fn(response);
+					}
+					return {
+						error: function (errorFn) {
+							if (user !== 'snaebjorn13' && pass === '123456') {
+								errorFn();
+							}
+						}
+					};
+				}
+			};
+		},
+
+		setUser: function (user) { },
+		setRole: function (pass) { },
+		setToken: function (token) { }
+	};
+
+	var mockLocation = {
+		path: function (location) { }
+	};
+
+	var mockEvaluationTemplateResource = {
+		create: function (templateObj) {
+			return {
+				success: function (fn) {
+					// call fn() if success
+					if (ID !== null &&
+						Title !== '' &&
+						TitleEn !== '' &&
+						IntroText !== '' &&
+						IntroTextEN !== '' &&
+						(CourseQuestions.length !== 0 ||
+						 TeacherQuestions.length !== 0))
+				}
+				return {
+					error: function (errorFn) {
+						// call errorFn() if error
+					}
+				};
+			};
+		}
+	};
+
 	beforeEach(inject(function (_$controller_) {
 		$controller = _$controller_;
 	}));
