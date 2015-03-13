13d5ec7b32f615564fa9c31a77af8c976e1c494a
Snaebjorn Eyjolfsson <snabbie1@gmail.com>
2015-03-13 18:19:35 +0000

    close to 100% code coverage in studentController

--- dist/app.js
+++ dist/app.js
@@ -237,34 +237,35 @@ function ($http, SERVER_URL) {
 angular.module('EvalApp').controller('StudentController',
 ['$scope', 'LoginResource', 'MyResource', '$location',
 function ($scope, LoginResource, MyResource, $location) {
-	$scope.token = LoginResource.getToken();
-	$scope.currentUser = LoginResource.getUser();
-	$scope.myCourses = [];
-	$scope.myEvaluations = [];
-	
-	if($scope.token !== '') {
+
+	if(LoginResource.isLoggedIn()) {
+		$scope.token = LoginResource.getToken();
+		$scope.currentUser = LoginResource.getUser();
+		$scope.myCourses = [];
+		$scope.myEvaluations = [];
+
 		MyResource.init($scope.token);  //Initialize token and config in MyResource
 		
 		MyResource.courses()
 		.success(function (response) {
 			$scope.myCourses = response;
-			toastr.success("Fetched courses");
+			toastr.success('Fetched courses');
 
 		})
 		.error(function () {
-			toastr.error("Something went wrong");
+			toastr.error('Something went wrong');
 		});
 
 		MyResource.evaluations()
 		.success (function (response) {
 			$scope.myEvaluations = response;
-			console.log("Eval: " + $scope.myEvaluations);
-			toastr.success("Fetched evaluations");
+			console.log('Eval: ' + $scope.myEvaluations);
+			toastr.success('Fetched evaluations');
 		})
 		.error (function () {
-			toastr.error("Could not fetch your evaluations");
+			toastr.error('Could not fetch your evaluations');
 		});
 	} else {
-		toastr.error("Token undefined");
+		toastr.error('No user logged in!');
 	}
 }]);
--- test/StudentController.spec.js
+++ test/StudentController.spec.js
@@ -1,40 +1,36 @@
 describe('StudentController', function () {
 	// this tests the logic of the StudentController
+	var scope, createController;
 
-	var $controller, $scope;
+	var mockLoginResourceWithToken = {
+		getUser: function () { return 'user'; },
+		getToken: function () { return 't0k3n'; },
+		isLoggedIn: function () { return true; }
+	};
 
-	var mockLoginResource = {
-		getUser: function (user) { },
-		getToken: function (token) { }
+	var mockLoginResourceNoToken = {
+		getUser: function () { return ''; },
+		getToken: function () { return ''; },
+		isLoggedIn: function () { return false; }
 	};
 
 	var mockMyResource = {
-		definedToken: false,
-
-		init: function (token) {
-			if (token !== '') {
-				this.definedToken = true;
-			}
-		},
+		init: function (token) { },
 
 		courses: function () {
 			return {
 				success: function (fn) {
 					// fn() if success
-					if (this.definedToken) {
-						var response = [
-							'Styrikerfi',
-							'Vefforritun',
-							'Gervigreind'
-						];
-						fn(response);
-					}
+					var response = [
+						'Styrikerfi',
+						'Vefforritun',
+						'Gervigreind'
+					];
+					fn(response);
 					return {
 						error: function (errorFn) {
 							// errorFn() if error
-							if (!this.definedToken) {
-								errorFn();
-							}
+								// errorFn();
 						}
 					};
 				}
@@ -45,27 +41,22 @@ describe('StudentController', function () {
 			return {
 				success: function (fn) {
 					// fn() if success
-					if (this.definedToken) {
-						var response = [
-							{ ID: 1,
-							  CourseID: 1,
-							  CourseName: 'Vefforritun',
-							  CourseNameEN: 'Web Programming',
-							  SemesterID: 1 },
-							{ ID: 2,
-							  CourseID: 2,
-							  CourseName: 'Styrikerfi',
-							  CourseNameEN: 'Operating Systems',
-							  SemesterID: 1 }
-						];
-						fn(response);
-					}
+					var response = [
+						{ ID: 1,
+						  CourseID: 1,
+						  CourseName: 'Vefforritun',
+						  CourseNameEN: 'Web Programming',
+						  SemesterID: 1 },
+						{ ID: 2,
+						  CourseID: 2,
+						  CourseName: 'Styrikerfi',
+						  CourseNameEN: 'Operating Systems',
+						  SemesterID: 1 }
+					];
+					fn(response);
 					return {
 						error: function (errorFn) {
 							// errorFn() if error
-							if (!this.definedToken) {
-								errorFn();
-							}
 						}
 					};
 				}
@@ -80,56 +71,71 @@ describe('StudentController', function () {
 	beforeEach(function () {
 		module('EvalApp');
 
-		inject(function (_$controller_) {
-			$controller = _$controller_;
+		inject(function ($controller, $rootScope) {
+			scope = $rootScope.$new();
+
+			createController = function (isToken) {
+				var mockLogin;
+				if (isToken) {
+					mockLogin = mockLoginResourceWithToken;
+				} else {
+					mockLogin = mockLoginResourceNoToken;
+				}
+		
+				return $controller('StudentController',
+					{ $scope: 		 scope,
+					  LoginResource: mockLogin,
+					  MyResource: 	 mockMyResource,
+					  $location: 	 mockLocation});
+			};
 		});
+
+		spyOn(mockLoginResourceWithToken, 'getUser');
+		spyOn(mockLoginResourceWithToken, 'getToken');
+		spyOn(mockLoginResourceNoToken, 'getUser');
+		spyOn(mockLoginResourceNoToken, 'getToken');
+		spyOn(mockLocation, 'path');
+		spyOn(mockMyResource, 'init');
+		spyOn(mockMyResource, 'courses').and.callThrough();
+		spyOn(mockMyResource, 'evaluations').and.callThrough();
+		spyOn(toastr, 'error');
+		spyOn(toastr, 'success');
 	});
 
 	describe('when the token is undefined', function () {
-		beforeEach(function () {
-			$scope = { };
 
-			spyOn(mockLoginResource, 'getUser');
-			spyOn(mockLoginResource, 'getToken');
-			spyOn(mockLocation, 'path');
-			spyOn(mockMyResource, 'init');
-			spyOn(mockMyResource, 'courses').and.callThrough();
-			spyOn(mockMyResource, 'evaluations').and.callThrough();
-			spyOn(toastr, 'error');
-			spyOn(toastr, 'success');
-
-			$scope.token = '';
-
-			$controller = $controller('StudentController',
-				{ $scope:        $scope,
-				  LoginResource: mockLoginResource,
-				  MyResource:    mockMyResource,
-				  $location:     mockLocation });
+		it('should not get token from LoginResource', function () {
+			var controller = createController(false);
+			expect(mockLoginResourceNoToken.getToken).not.toHaveBeenCalled();
+			expect(mockLoginResourceNoToken.getUser).not.toHaveBeenCalled();
 		});
 
-		it('should get token and from LoginResource', function () {
-			expect(mockLoginResource.getToken).toHaveBeenCalled();
-			expect(mockLoginResource.getUser).toHaveBeenCalled();
+		it('should alert with toastr if token is undefined', function () {
+			var controller = createController(false);
+			// expect(scope.token).toEqual('');
+			expect(toastr.error).toHaveBeenCalledWith('No user logged in!');
 		});
-
-		/* it('should alert with toastr if token is undefined', function () {
-			expect(toastr.error).toHaveBeenCalledWith('Token undefined');
-		}); */
 	});
 
-	/* describe('when toastr is defined', function () {
+	describe('when the token is defined', function () {
 
 		it('should call init with token if token is defined', function () {
-			$scope.token = 't0k3n';
-			expect(mockMyResource.init).toHaveBeenCalled();
+			var controller = createController(true);
+			expect(mockMyResource.init).toHaveBeenCalledWith(scope.token);
 		});
 
 		it('should call courses if token is defined', function () {
+			var controller = createController(true);
 			expect(mockMyResource.courses).toHaveBeenCalled();
+			expect(scope.myCourses.length).toEqual(3);
+			expect(toastr.success).toHaveBeenCalledWith('Fetched courses');
 		});
 
 		it('should call evaluations if token is defined', function () {
+			var controller = createController(true);
 			expect(mockMyResource.evaluations).toHaveBeenCalled();
+			expect(scope.myEvaluations.length).toEqual(2);
+			expect(toastr.success).toHaveBeenCalledWith('Fetched evaluations');
 		});
-	}); */
+	});
 });
