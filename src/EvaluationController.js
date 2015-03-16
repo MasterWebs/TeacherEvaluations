d24e2aedd62b2e94e928f16fad862efba2f1f174
omaroskars <omar13@ru.is>
2015-03-16 20:04:44 +0000

    something with evaluations

--- src/EvaluationController.js
+++ src/EvaluationController.js
@@ -1,14 +1,21 @@
 angular.module('EvalApp').controller('EvaluationController',
 ['$scope', '$location', 'LoginResource', 'EvaluationResource',
 function ($scope, $location, LoginResource, EvaluationResource) {
-	$scope.evaluation = EvaluationResource.getThisEvaluation();
-	console.log($scope.evaluation.Courses);
-
-	EvaluationResource.getEvaluation($scope.evaluation.ID)
-	.success(function () {
-
-	})
-	.error(function () {
-		toastr.error("Could not fetch evaluation");
-	});
+	$scope.role = LoginResource.getRole();
+	$scope.thisEvaluation = EvaluationResource.getThisEvaluation();
+	$scope.evaluation = [];
+	
+	if($scope.role !== 'admin') {
+		toastr.error('Please login as admin');
+		$location.path('/login');
+	} else {
+		EvaluationResource.getEvaluation($scope.thisEvaluation.ID)
+		.success(function (response) {
+			console.log(response);
+			$scope.evaluation = response;
+		})
+		.error(function () {
+			toastr.error("Could not fetch evaluation");
+		});
+	}
 }]);
