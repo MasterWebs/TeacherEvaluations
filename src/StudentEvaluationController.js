571dced98bce16dff4631d1b64535f0a4558b96d
Snaebjorn Eyjolfsson <snabbie1@gmail.com>
2015-03-17 23:57:49 +0000

    posting an evaluation is on its way

--- src/StudentEvaluationController.js
+++ src/StudentEvaluationController.js
@@ -3,7 +3,8 @@ angular.module('EvalApp').controller('StudentEvaluationController',
 function ($scope, $location, LoginResource, CourseResource) {
 	$scope.role = LoginResource.getRole();
 	$scope.courseEvaluation = CourseResource.getThisEvaluation();
-	//$scope.course = CourseResource.getThisCourse(); DONT NEED I THINK
+	$scope.course = CourseResource.getThisCourse();
+	$scope.semester = 1;
 	$scope.evaluation = {};	
 	$scope.courseQuestions = [];
 	$scope.teacherQuestions = [];
@@ -48,5 +49,27 @@ function ($scope, $location, LoginResource, CourseResource) {
 
 	$scope.submitEvaluation = function () {
 		$scope.results = [];
+
+		angular.forEach($scope.courseQuestions, function (obj) {
+			var ans = { QuestionID: obj.question.ID, Value: '' };
+			if (obj.question.Type === 'text' || obj.question.Type === 'single') {
+				ans.Value = obj.answer;
+			} else if (obj.question.Type === 'multiple') {
+				// dunno, do it later man
+			}
+			this.push(ans);
+		}, $scope.results);
+
+		angular.forEach($scope.teacherQuestions, function (obj) {
+			var ans = { QuestionID: obj.question.ID, Value: '' };
+			if (obj.question.Type === 'text' || obj.question.Type === 'single') {
+				ans.Value = obj.answer;
+			} else if (obj.question.Type === 'multiple') {
+				// dunno, do it later man
+			}
+			this.push(ans);
+		}, $scope.results);
+
+		// TODO: Post results to server
 	};
 }]);
